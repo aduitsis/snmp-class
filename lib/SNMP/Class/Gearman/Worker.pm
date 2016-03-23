427c715fa7c2051bfcb4f96b03156aee7a84a211
Athanasios Douitsis <aduitsis@cpan.org>
2016-03-23 21:46:56 +0200

    better signal handling in Worker
    Replaced the two signal handlers with a foor loop, better. Also,
    inside a signal handler, when the signal is set to IGNORE, the
    handler variable was declared local, so after the handler did
    die or exit, the handler was reset to its original. That resulted
    in signals caught two times, e.g. when the user typed Ctrl-C,
    there was a SIGINT from the shell (all members in the process
    group get it) and then there was a signal from the master itself
    to the worker. So that produced some nasty warnings while dying.
    Anyway, fixed by removing the local qualifier. Now IGNORE is there
    to stay (albeit not for long since the worker is dying).

--- lib/SNMP/Class/Gearman/Worker.pm
+++ lib/SNMP/Class/Gearman/Worker.pm
@@ -2,7 +2,7 @@ package SNMP::Class::Gearman::Worker;
 
 use v5.14;
 
-use Exporter 'import'; 
+use Exporter 'import';
 our @EXPORT_OK = qw(generate_worker spawn_worker gather);
 
 use strict;
@@ -14,30 +14,30 @@ use Scalar::Util;
 use Gearman::Worker;
 use YAML;
 use Moose::Util qw/find_meta does_role search_class_by_role/;
-use Log::Log4perl;
 use Data::Printer;
 use Storable;
 use JSON;
 
-my $logger = Log::Log4perl::get_logger();
+use Log::Log4perl;
+my $logger = Log::Log4perl->get_logger();
 
 =head2 spawn_worker( \@job_servers , $function_name , $id, $json )
 
 Spawns a worker process that will generate a gearman worker and attach
 it to a gearman server. So it basically fork()s and then registers a
-worker with a gearman job server. 
+worker with a gearman job server.
 
 The \@job_servers is a reference to an array of
 job server definitions. See how the job_servers method is used with
-L<Gearman::Worker>. 
+L<Gearman::Worker>.
 
-The $function name is the name by which the worker will register itself as a 
-function with gearman. 
+The $function name is the name by which the worker will register itself as a
+function with gearman.
 
 The $id is an identifier (e.g. an integer) that will identify this
 specific worker that will be spawned.
 
-If the $json parameter is true, the worker will return its results in JSON format 
+If the $json parameter is true, the worker will return its results in JSON format
 instead of YAML. If missing, defaults to false.
 
 =cut
@@ -54,23 +54,21 @@ sub spawn_worker {
 	return $pid if($pid > 0);
 
 	#now we are the child. We can setup a friendly signal handler
-	$SIG{INT} = sub {
-		# ignore any repetitions of the signal
-		local $SIG{INT}='IGNORE';
-		die "Child $id caught signal SIGINT ... exiting";
-	};
-	$SIG{TERM} = sub {
-		# ignore any repetitions of the signal
-		local $SIG{TERM}='IGNORE';
-		die "Child $id caught signal SIGTERM ... exiting";
-	};
+	for my $signal ('INT','TERM') {
+		$SIG{$signal} = sub {
+			# ignore any repetitions of the signal
+			$SIG{$signal}='IGNORE';
+			$logger->info("worker $id caught signal SIG$signal ... exiting");
+			exit
+		};
+	}
 
 	#also change the process name
 	$0 = "omnidisco worker $id $function_name";
 
 	my $worker = Gearman::Worker->new;
 	$worker->job_servers(@{$job_servers});
-	$worker->register_function( $function_name => generate_worker($id, $json)); #keep in mind, gather_worker returns a sub
+	$worker->register_function( $function_name => generate_worker($id, $json)); #keep in mind, generate_worker returns a sub
 	$logger->info( "worker $id connected to ".join(',',@{$job_servers}).', ready to work' );
 	$worker->work while 1;
 }
@@ -78,7 +76,7 @@ sub spawn_worker {
 =head2 generate_worker( $id )
 
 Returns a worker that is suitable to pass to gearman. $id is used as
-an identifier of the specific worker e.g. for log messages etc. 
+an identifier of the specific worker e.g. for log messages etc.
 
 =cut
 
@@ -89,11 +87,11 @@ sub generate_worker {
 	#construct a sub (closure) and return it
 	return sub {
 		my $arg;
-		
+
 		if( $json ) {
 			my $incoming_args = decode_json shift->arg;
 			@{$arg} = %{ $incoming_args }
-		} 
+		}
 		else {
 			$arg = Load(shift->arg) #unserialize the arguments
 		}
@@ -114,7 +112,7 @@ sub generate_worker {
 =head2 gather
 
 Gathers all available data from an SNMP agent and returns
-whatever facts can be obtained. 
+whatever facts can be obtained.
 
 =cut
 
@@ -138,13 +136,13 @@ sub gather {
 	my $query_all_vlans = 0;
 	if( exists( $args{ query_all_vlans } ) && $args{ query_all_vlans } ) {
 		$query_all_vlans = 1;
-	}	
+	}
 
 	my $return_json;
 	if( exists( $args{ json } ) && $args{ json } ) {
 		$return_json = 1;
 		$logger->debug('will return JSON');
-	}	
+	}
 
 
 	#for( @{ $s->fact_set->facts } ) {
@@ -156,8 +154,8 @@ sub gather {
 
 
 	if( $s->vendor eq 'cisco' ) {
-		my ( $method, $personality ); 
-		if( $query_all_vlans && does_role( $s , 'SNMP::Class::Role::Personality::VtpVlanState' ) ) { 
+		my ( $method, $personality );
+		if( $query_all_vlans && does_role( $s , 'SNMP::Class::Role::Personality::VtpVlanState' ) ) {
 			$method = 'get_vtp_vlans';
 			$personality = 'SNMP::Class::Role::Personality::VtpVlanState';
 		}
@@ -167,8 +165,8 @@ sub gather {
 		}
 		else {
 			$logger->warn('No available way to query vlans')
-		} 
-			
+		}
+
 		if( $method ) {
 			my $original_community = $s->community;
 			for( $s->$method ) {
@@ -189,9 +187,9 @@ sub gather {
 	$logger->info('calculating facts');
 	$s->calculate_facts;
 
-	$s->fact_set->push( SNMP::Class::Fact->new( 
+	$s->fact_set->push( SNMP::Class::Fact->new(
 		type	=> 'gather_meta',
-		slots	=> { 
+		slots	=> {
 			start_time	=> $start_time,
 			stop_time	=> $stop_time,
 			target		=> $s->hostname,
@@ -204,7 +202,7 @@ sub gather {
 			engine_id	=> $s->engine_id,
 		},
 	));
-			
+
 	for( @{ $s->fact_set->facts } ) {
 		$logger->debug($_->to_string);
 	}
